fc52a2a14a3943618bf0291a95d7ea025150a625
Konstantin Sukharev <poed@me.com>
2025-08-17 16:03:19 +0400

    Remove 6 bolt design

--- agc.py
+++ agc.py
@@ -2,140 +2,107 @@ from math import sin, cos, pi, floor, sqrt
 from typing import Union, Tuple, Sequence, Optional, Callable, List, Dict, TypeVar, Any
 from functools import reduce
 from cadquery import exporters, Vector, Workplane
+from ocp_vscode import show
 
 import sys, os
 sys.path.append(os.path.abspath("."))
-from lib.ddd import lemo, grid
+from lib.ddd import *
 from lib.tools import *
 
-wt: float = 3.3
-wt2: float = 2.0
+m4xr: float = 4.1 / 2
+m4dr: float = 3.3 / 2
 
-cw: float = 101.0
-ch: float = 161.0
-w: float = cw + 9.0 * 2
-h: float = ch + wt * 2
+wt: float = 3.0
+wt2: float = 1.5
+
+cw: float = 4 * inch
+ch: float = 6 * inch
+col: float = 12.0
+hol: float = col / 2
+w: float = 4.5 * inch
+h: float = 7 * inch
 t: float = 30.0
 t2: float = t / 2.0
-col: float = 9.0
-hol: float = col / 2
+t3: float = t2 / 2.0
+c1: float = 1.0
+c2: float = 0.5
 
-c: float = 2.5
-c2: float = 0.25
-
-cell: float = inch
-scuth: float = (h - wt * 6) / 2 - wt * 2
-
-wp = Workplane()
-
-def holes(w: float, h: float, d: float, l: float, r: float) -> Workplane:
-    return sum([
-        wp.cylinder(l, r).translate((w / 2 - d, h / 2 - d, 0)),
-        wp.cylinder(l, r).translate((-w / 2 + d, h / 2 - d, 0)),
-        wp.cylinder(l, r).translate((w / 2 - d, -h / 2 + d, 0)),
-        wp.cylinder(l, r).translate((-w / 2 + d, -h / 2 + d, 0)),
-        wp.cylinder(l, r).translate((w / 2 - d, 0, 0)),
-        wp.cylinder(l, r).translate((-w / 2 + d, 0, 0)),
-    ])
+def box_fc(w, h, t, fe, fr, c) -> Workplane:
+	return (
+        wp.box(w, h, t)
+		.edges(fe).fillet(fr)
+		.edges().chamfer(c)
+	)
 
 def hcuts(w: float, h: float, d: float, l: float, r: float) -> Workplane:
     return sum([
-        (
-            wp.box(r * 4, r * 4, l + c2 * 2)
-            .edges("+Z")
-            .fillet(r)
-            .edges()
-            .chamfer(c2)
-            .translate((w / 2 - d + r, h / 2 - d + r, c2))
-            .mirror("YZ", union = True)
-            .mirror("XZ", union = True)
+        mirror2('XZ', 'YZ') (
+            box_fc((r + d) * 2, (r + d) * 2, l + c1, "+Z", r, c2)
+            .translate((w / 2, h / 2, c2 - l / 2))
         ),
-        (
-            wp.box(r * 4, r * 2 + pl, l + c2 * 2)
-            .edges("+Z")
-            .fillet(r)
-            .edges()
-            .chamfer(c2)
-            .translate((w / 2 - d + r, 0, c2))
-            .mirror("YZ", union = True)
-        ),
-        (
-            wp.box(r * 4 + c2 * 2, r * 4 + c2 * 2, c2 * 2 + pl)
-            .edges("+Z")
-            .fillet(r + c2)
-            .edges()
-            .chamfer(c2)
-            .translate((w / 2 - d + r, h / 2 - d + r, l / 2))
-            .mirror("YZ", union = True)
-            .mirror("XZ", union = True)
-        ),
-        (
-            wp.box(r * 4 + c2 * 2, r * 2 + pl + c2 * 2, c2 * 2 + pl)
-            .edges("+Z")
-            .fillet(r + c2)
-            .edges()
-            .chamfer(c2)
-            .translate((w / 2 - d + r, 0, l / 2))
-            .mirror("YZ", union = True)
+        mirror2('XZ', 'YZ') (
+            box_fc((r + d + c2) * 2, (r + d + c2) * 2, c1 + pl, "+Z", r + c2, c2)
+            .translate((w / 2, h / 2, 0))
         ),
     ])
 
-def brick(side: float = 1.0) -> Workplane:
+def brick(wall: float, dir: float) -> Workplane:
     return dif([
         (
-            wp.box(w, h, t2).edges("+Z").chamfer(c)
+            wp.box(w, h, t2)
+            .edges("+Z").chamfer(wt2 * 3)
             .intersect(
-                wp.box(w, h, t2 + c2 * 2)
-                .edges("+Z").chamfer(c)
-                .edges().chamfer(c2)
-                .translate((0, 0, c2 * side))
+                wp.box(w, h, t2 + c1)
+                .edges("+Z").chamfer(wt2 * 3)
+                .edges("not +Z").chamfer(c2)
+                .translate((0, 0, c2 * dir))
             )
         ),
-        (
-            wp.box(cw, ch, t2)
-            .edges("|Z")
-            .fillet(2.0)
-            .edges()
-            .chamfer(c2 * 2)
-            .translate((0, 0, wt2 * side))
-        ),
-        (
-            wp.box((col - wt) * 2, scuth, t2)
-            .edges("|X")
-            .fillet(2.0)
-            .edges()
-            .chamfer(c2 * 2)
-            .translate((w / 2, h / 4 - col / 4, wt * side))
-            .mirror("YZ", union = True)
-            .mirror("XZ", union = True)
+        com2(mov(0, 0, wall * dir), sum) ([
+            box_fc(w - col, h - col * 2, t2, "|Z", 2.0, c2),
+            box_fc(w - col * 2, h - wt * 2, t2, '|Z', 2.0, c2),
+        ]),
+        mirror("YZ") (
+            box_fc(wt * 2, h - col * 2, t2 + 4.0, '|X', 2.0, c2)
+            .translate((w / 2, 0, -wt - 2.0 if dir != 1 else 0))
         ),
     ])
 
-def case() -> Workplane:
-    return sum([
-        dif([
-            brick(1.0),
-            holes(w, h, hol, t2, 2.5 / 2).translate((0, 0, 0)),
-            lemo(wt).rotate((0, 0, 0), (1, 0, 0), 90).translate((-inch, h / 2 - wt / 2, wt2 / 2)),
-            lemo(wt).rotate((0, 0, 0), (1, 0, 0), 90).translate((inch, h / 2 - wt / 2, wt2 / 2)),
-        ]),
-    ]).translate((0, 0, t2 / 2))
+def makeTop(ptn: Callable[[int, int], bool]) -> Workplane:
+    hole_large = wp.cylinder(t2, 9.53 / 2 + 0.1)
+    hole_small = wp.cylinder(t2, inch / 8 + 0.1)
+    return com2(mov(0, 0, t3), dif) ([
+        brick(wt, -1.0),
+        grid(ptn_map(ptn, lambda: hole_large, lambda: hole_small)),
+        holes(w, h, hol, t2, m4xr),
+        hcuts(w, h, hol, wt * 2, hol * s2).translate((0, 0, t3)),
+        com(mirror("XZ"), mov(0, 1.5 * inch, t3)) (
+            box_fc(cw - wt, 3 * inch - wt, 2.0, '|Z', 2.0, c2)
+        ),
+    ])
 
-def makePanel(ptn: Callable[[int, int], bool]) -> Workplane:
-    return dif([
-        brick(-1.0),
-        grid(lambda x, y: (
-            wp.cylinder(t2, inch / 8 + 0.2)
-            if ptn(x, y) and y < 3 else
-            wp.cylinder(t2, 9.7 / 2)
-        )),
-        holes(w, h, hol, t2, 3.1 / 2),
-        hcuts(w, h, hol, wt, wt).translate((0, 0, t2 / 2 - wt / 2)),
-    ]).translate((0, 0, t2 / 2))
+def makeBot() -> Workplane:
+    return com(mov(0, 0, t3), dif) ([
+        brick(wt, 1.0),
+        holes(w, h, hol, t2, m4dr),
+        mov(0, 0, -t3 / 2) (holes(w, h, hol, t3, 4.5 / 2)),
+        com(mirror("XZ"), mov(0, (h / 2 - col + wt2) / 2, wt - t3)) (
+            box_fc(w - 4 * wt, h / 2 - col - wt2, 2.0, '|Z', 2.0, c2)
+        ),
+        com(mirror("XZ"), mov(0, (h - col) / 2 - wt, wt - t3)) (
+            box_fc(w - col * 2, col, 2.0, '|Z', 2.0, c2),
+        ),
+        com(mirror("YZ"), mov(inch, h / 2 - wt / 2, 1.0)) (
+             lemo(wt).rotate((0, 0, 0), (1, 0, 0), 90)
+        ),
+    ])
 
-top = makePanel(pattern("<>"))
-bot = case()
-comp = bot + top.translate((0, 0, t2))
+bot = makeBot()
+top = makeTop(ptn_all)
+
+comp = bot + top.translate((0, 0, t2 + pl))
+
+# show(bot, top.translate((0, 0, t2 + pl)))
 
 exporters.export(top, "STL/AGC01.stl")
 exporters.export(bot, "STL/AGC10.stl")
