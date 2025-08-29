1968b5039b0b2e197306f73fed60ee13553171d6
Konstantin Sukharev <poed@me.com>
2025-08-29 17:15:52 +0400

    Fix export

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ offsets: Callable[[int], Callable[[Workplane], Workplane]] = lambda m: mov(
 	7.5 * inch if m == 2 else 4 * inch if m == 3 else 0,
 	8 * inch if m == 3 else 0
 )
+
 units: List[List[Workplane]] = [
 	map_lst(offsets(m)) (
 		agc(
@@ -21,10 +22,12 @@ units: List[List[Workplane]] = [
 	for m in range(1, 4)
 ]
 
-# for i, parts in enumerate(units):
-# 	export(f'AGC-{i}M-01', parts[0])
-# 	export(f'AGC-{i}M-10', parts[1])
-# 	export(f'AGC-{i}M-11', sum([parts[0], parts[1]]), step = False)
-# 	if i == 1 and len(parts) > 4: export(f'AGC-01T', parts[4], stl = False, step = False)
+for i, parts in enumerate(units):
+	export(f'AGC-{i + 1}M-01', parts[0])
+	export(f'AGC-{i + 1}M-10', parts[1])
+	export(f'AGC-{i + 1}M-11', sum([parts[0], parts[1]]), step = False)
+	if i == 0 and len(parts) > 4: export(f'AGC-01T', parts[4], stl = False, step = False)
 
 show(units)
+
+# show(knob())
